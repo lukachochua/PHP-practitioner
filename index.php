e523410b0619ea0650f2ada3308317d6d8f3a007
Luka Chochua <lukachochua@gmail.com>
2022-07-13 00:35:21 +0400

    new

--- index.php
+++ index.php
@@ -1,5 +1,6 @@
 <?php
 
+require 'vendor/autoload.php';
 require 'functions.php';
 
 require 'core/bootstrap.php';
---
+++ vendor/composer/installed.php
@@ -0,0 +1,24 @@
+<?php return array (
+  'root' => 
+  array (
+    'pretty_version' => 'dev-master',
+    'version' => 'dev-master',
+    'aliases' => 
+    array (
+    ),
+    'reference' => '9316b20c6c148454692bb7341d559ee68ae52ee1',
+    'name' => '__root__',
+  ),
+  'versions' => 
+  array (
+    '__root__' => 
+    array (
+      'pretty_version' => 'dev-master',
+      'version' => 'dev-master',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '9316b20c6c148454692bb7341d559ee68ae52ee1',
+    ),
+  ),
+);
